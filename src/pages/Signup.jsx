b85099316b0cc90c07daf10db7b87e3ff858655b
vectar-labs <abdulazizsadi31@gmail.com>
2026-05-14 12:58:06 +0300

    added form validation with react-hook form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,31 +1,32 @@
-import { useForm } from "react-hook-form";
+import React from "react";
 
-const Auth = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
+const Signup = () => {
   return (
     <div className="w-full min-h-screen flex items-center justify-center ">
-      <form className="w-full max-w-xs mx-auto" onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-5 w-full">
+      <form className="w-full max-w-xs mx-auto">
+        <div className="mb-5">
+          <label htmlFor="email" className="block mb-2.5 text-sm font-medium text-heading">
+            Your name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+            placeholder="John Doe"
+            required
+          />
+        </div>
+        <div className="mb-5">
           <label htmlFor="email" className="block mb-2.5 text-sm font-medium text-heading">
             Your email
           </label>
           <input
             type="email"
             id="email"
-            {...register("email", { required: true })}
             className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
             placeholder="name@flowbite.com"
+            required
           />
-          <p className="text-xs text-red-400 mt-3">{errors.email && "Email is required"}</p>
         </div>
         <div className="mb-5">
           <label htmlFor="password" className="block mb-2.5 text-sm font-medium text-heading">
@@ -34,24 +35,23 @@ const Auth = () => {
           <input
             type="password"
             id="password"
-            {...register("password", { required: true, minLength: 6, maxLength: 20 })}
             className="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
             placeholder="••••••••"
+            required
           />
-          <p className="text-xs text-red-400 mt-3">{errors.password && "Password is required it should be between 6 and 20 characters"}</p>
         </div>
 
         <button
           type="submit"
           className="text-white bg-teal-600 w-full box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none cursor-pointer"
         >
-          Login
+          Signup
         </button>
         <label htmlFor="remember" className="flex items-center my-5">
           <p className="ms-2 text-sm font-medium text-heading select-none">
-            Dont have an account?{" "}
-            <a href="/auth/signup" className="text-fg-brand hover:underline">
-              Sign up
+            Already have an account?{" "}
+            <a href="/auth" className="text-fg-brand hover:underline">
+              Login
             </a>
           </p>
         </label>
@@ -60,4 +60,4 @@ const Auth = () => {
   );
 };
 
-export default Auth;
+export default Signup;
